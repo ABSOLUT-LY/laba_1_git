6f45bc0166ed1e232c32c485ab182384988a289b
ABSOLUT-LY <donskoy.denis.2007@gmail.com>
2025-09-04 19:32:40 +0300

    Теперь всё работает. 1.1.1 - сделано

--- main.cpp
+++ main.cpp
@@ -1,110 +1,117 @@
 #include <iostream>
+#include <cstdlib>
+#include "laba_1.1.1.h"
 
 using namespace std;
 
-int n;
+// ФУНКЦИИ ДЛЯ UI
 
-void error_message_zero_count(int zc)
+// oчистка терминала
+void clear_terminal()
 {
-    cout << "Ошибка, найдено " << zc << " нулей, ожидалось 2" << endl;
+    system("cls");
 }
 
-void error_message_zero_index(int zc)
+// диалог для возврата на главный экран
+void homescreen_message()
 {
-    cout << "Ошибка, между нулями " << zc << " элемент(а), ожидалось 2 и более" << endl;
+    char letter;
+    while (true)
+    {
+        cout << endl
+             << "Do you want return on main screen [Y/N]?" << endl;
+        cin >> letter;
+             switch (letter)
+        {
+        case 'Y':
+            clear_terminal();
+            return;
+        }
+    }
 }
 
-template <typename T>
-bool valid(T *arr)
+// функция для действия 1 задания 1 в терминале
+void process_1_terminal()
 {
-    int ind_zero = 0;
-    int zero_count = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] == 0 && not(zero_count))
-        {
-            zero_count++;
-            ind_zero = i;
-        }
-        else if (arr[i] == 0 && zero_count)
-        {
-            zero_count++;
-            ind_zero = i - ind_zero;
-        }
-    }
-    if (zero_count != 2 && ind_zero < 2)
-    {
-        error_message_zero_count(zero_count);
-        error_message_zero_index(ind_zero);
-        return false;
-    }
-    else
-    {
-        if (zero_count != 2)
-        {
-            error_message_zero_count(zero_count);
-            return false;
-        }
-        if (ind_zero < 2)
-        {
-            error_message_zero_index(ind_zero);
-            return false;
-        }
-    }
-    return true;
+    clear_terminal();
+    Exercise_1 ex_1;
+    clear_terminal();
+    ex_1.input_dimension();
+    clear_terminal();
+    ex_1.input_arr();
+    clear_terminal();
+    ex_1.output_actions_for_process_1();
+    homescreen_message();
 }
 
-
-
-template <typename T>
-T *input_arr(T *arr)
+// функция для действия 2 задания 1 в терминале
+void process_2_terminal()
 {
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-    return arr;
 }
 
-template <typename T>
-T sum_arr(T *arr)
+// функция для действия 3 задания 1 в терминале
+void process_3_terminal()
 {
-    bool flag_zero = false;
-    T sum = 0;
-    for (int i = 0; i < n; i++)
+}
+
+// задание 1
+void screen_exercise_1_terminal()
+{
+    clear_terminal();
+    int k = 0; // Номер оперцаии
+    cout << "1" << "\t" << "Calculate the sum of negative elements of the array, determine their number and find their coordinates;" << endl;
+    cout << "2" << "\t" << "Calculate the product of array elements located between the maximum and minimum element, indicating their position in the array;" << endl;
+    cout << "3" << "\t" << "Sort array elements in ascending order" << endl;
+    cout << "Select and enter the number of the operation you need:" << "\t";
+    while (true)
     {
-        if (arr[i] == 0)
+        cin >> k;
+        switch (k)
         {
-            flag_zero = true;
-        }
-        if (arr[i] == 0 && flag_zero)
-        {
-            if (sum)
-            {
-                return sum;
-            }
-        }
-        if (flag_zero)
-        {
-            sum += arr[i];
+        case 1:
+            process_1_terminal();
+            return;
+        case 2:
+            return;
+        case 3:
+            return;
+        default:
+            cout << "Invalid operation number. Try again:" << "\t";
+            break;
         }
     }
-    return 0;
+}
+
+// функция для создания и работы с программой
+void terminal_process()
+{
+    int k = 0; // Номер задания
+    cout << "Welcome to the program!" << endl;
+    cout << "1. Exercise 1;" << endl;
+    cout << "2. Exercise 2;" << endl;
+    cout << "3. Exercise 3;" << endl;
+    cout << "Select and enter the task number:" << "\t";
+    while (true)
+    {
+        cin >> k;
+        switch (k)
+        {
+        case 1:
+            screen_exercise_1_terminal();
+            terminal_process();
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        default:
+            cout << "Invalid operation number. Try again:" << "\t";
+            break;
+        }
+    }
 }
 
 int main()
-
 {
-    cin >> n;
-    int *arr = new int[n];
-    input_arr(arr);
-    if (not(valid(arr)))
-    {
-        return 0;
-    }
-    else
-    {
-        cout << sum_arr(arr);
-        return 0;
-    }
+    terminal_process();
 }
