9de71ce68fe6247af620e80b31aff9af868a270d
ABSOLUT-LY <donskoy.denis.2007@gmail.com>
2025-09-10 04:30:34 +0300

    Небольшие штрихи с памятью

--- result_array_worker_struct.hpp
+++ result_array_worker_struct.hpp
@@ -6,5 +6,9 @@ struct Arr_and_size
     T *arr;
     int size;
     Arr_and_size() : arr(nullptr), size(0) {};
-    Arr_and_size(T* array, int n) : arr(array), size(n) {};
+    Arr_and_size(T *array, int n) : arr(array), size(n) {};
+    ~Arr_and_size()
+    {
+        delete[] arr;
+    }
 };
